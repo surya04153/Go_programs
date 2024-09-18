6b3e8f9a1de4d7258f0686f7db3ddda580f4efe6
suryapanda <suryanarayan.panda@gmail.com>
2024-09-18 13:16:06 -0700

    Restrucred the folders.

---
+++ poem/Poem.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(`
+	In a world of semicolons, wide and deep,
+Where threads and locks put code to sleep,
+Came Go, a language light and swift,
+To give the coder’s world a lift.
+
+No more waits for builds so slow,
+Concurrency flows, like rivers in Go.
+With goroutines dancing, light as air,
+Parallel tasks get done with care.
+
+No classes, no inheritance tree,
+Just structs and interfaces, simple and free.
+The garbage collector sweeps with grace,
+While your code speeds on, a steady pace.
+
+Error handling, though plain and bare,
+Ensures your programs handle with care.
+A compiled friend, it runs so fast,
+Through problems large, it’ll always last.
+
+So here’s to Go, the language bright,
+That turns our code from day to night.
+In simplicity, it finds its might,
+With Go, the future shines so light.
+	`)
+}
---
+++ poem/go.mod
@@ -0,0 +1,3 @@
+module Poem
+
+go 1.23.1
