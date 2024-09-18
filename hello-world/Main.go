1646ee421357e2b22145ff38f77e81c13130b85a
suryapanda <suryanarayan.panda@gmail.com>
2024-09-18 13:25:05 -0700

    updated to explain comments and main function

--- hello-world/Main.go
+++ hello-world/Main.go
@@ -2,7 +2,16 @@ package main
 
 import "fmt"
 
+/* This is a hello-world program explaining
+    the
+	a)types of comment in go
+	b)Entry point for go application
+	c)Print function
+
+*/
+//main() function is the entry point of Go application
 func main() {
+	/* fmt package contains the function for printing*/
 	fmt.Println("hello world Go!!!!")
 
 }
